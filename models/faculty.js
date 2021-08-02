30933d71b4cd5ce96fc5ad651b654cb768c19d8d
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-02 18:32:02 +0300

    added password methods

--- models/faculty.js
+++ models/faculty.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const modelName = 'Faculty';
 
@@ -19,6 +20,32 @@ const FacultySchema = mongoose.Schema({
     academic_rank: { type: String, default: null },
     chair_membership: { type: String, default: null },
     adminstrative_role: { type: String, default: null },
+
+    // auth fields
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
 });
+
+FacultySchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    if (user.password.length < 6) {
+        return next(new Error('Password too short'));
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(process.env.SALT_FACTOR);
+        user.password = await bcrypt.hash(user.auth.password.trim(), salt);
+
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+FacultySchema.methods.isPasswordCorrect = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model(modelName, FacultySchema);
