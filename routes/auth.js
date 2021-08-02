f29aa6162ccc4f89a74bfb890a3b60d4fb56fc16
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-02 22:11:18 +0300

    added auth endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,86 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const FacultySchema = require('../models/faculty');
 
-router.post('/signup', (req, res) => res.end('reached'));
-router.post('/login', (req, res) => res.end('reached'));
-router.put('/profile', (req, res) => res.end('reached'));
-router.patch('/change_password', (req, res) => res.end('reached'));
+const jwtCrtl = require('../helpers/jwt');
+
+const { authenticateUser, is } = require('../middlewares/auth');
+const { failure, success } = require('../helpers/response');
+const { SUPER_ADMIN, FACULTY } = require('../constants/roles');
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return failure(res, 'username and password are required');
+    }
+
+    try {
+        const user = await FacultySchema.findByUsername(username);
+        const bearerToken = await jwtCrtl.createToken({ userId: user._id }, process.env.ENCR_SECRET);
+
+        return success(res, { token: bearerToken });
+    } catch (err) {
+        console.log(err);
+        return failure(res, 'Invalid login');
+    }
+});
+
+router.post('/signup', authenticateUser, is(SUPER_ADMIN), async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return failure(res, 'username and password required');
+    }
+
+    try {
+        const newUser = await FacultySchema.create(req.body);
+
+        return success(res, newUser);
+    } catch (err) {
+        return failure(res, 'Something went wrong');
+    }
+});
+
+router.put('/profile', authenticateUser, async (req, res) => {
+    const newProfile = req.body;
+
+    try {
+        if (!req.user.roles.includes(SUPER_ADMIN)) {
+            delete newProfile.roles;
+        }
+
+        await req.user.updateProfile(newProfile);
+        return success(res, req.user);
+    } catch (err) {
+        return failure(res, 'Something went wrong');
+    }
+});
+
+router.patch('/change_password', authenticateUser, is(FACULTY), async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        return failure(res, 'oldPassword and newPassword required');
+    }
+
+    if (oldPassword == newPassword) {
+        return failure(res, 'please use a different password');
+    }
+
+    try {
+        if (!req.user.isPasswordCorrect(oldPassword)) {
+            return failure(res, 'oldPassword is incorrect');
+        }
+
+        req.user.password = newPassword;
+        await req.user.save();
+
+        return success(res, { message: 'password updated successfuly' });
+    } catch (err) {
+        return failure(res, err.message || "Something went wrong");
+    }
+});
 
 module.exports = router;
