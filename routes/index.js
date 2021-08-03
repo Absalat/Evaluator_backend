a2183422d5990567fe41f9bccf80221a3a9ec22c
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-03 17:40:32 +0300

    added faculty evaluation endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const { FacultySelfEvaluationModel } = require('../models');
 
-const { authenticateUser } = require('../middlewares/auth');
+const { authenticateUser, is } = require('../middlewares/auth');
+const { failure, success } = require('../helpers/response');
+const { FACULTY } = require('../constants/roles');
 
 const router = express.Router();
 
-router.post('/faculty_evaluation', authenticateUser, (req, res) => {
-    res.end('reached');
+router.post('/faculty', authenticateUser, is(FACULTY), async (req, res) => {
+    try {
+        req.body.user = req.user._id;
+        const evaluation = await FacultySelfEvaluationModel.create(req.body);
+        return success(res, evaluation);
+    } catch (err) {
+        return failure(res, err);
+    }
 });
 
 module.exports = router;
