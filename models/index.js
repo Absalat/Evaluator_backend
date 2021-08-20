9f9510d57aa40b9c6ba88f09e5b809166e021333
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-20 11:16:18 +0300

    exporting chair self evaluation model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     FacultyModel: require('./faculty'),
     FacultySelfEvaluationModel: require('./faculty_self_evaluation'),
+    ChairSelfEvaluationModel: require('./chair_self_evaluation'),
 }
