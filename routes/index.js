82d18bcb11fee150ff9b52908e1785553b547ba0
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-20 11:16:54 +0300

    added chair evaluation endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { FacultySelfEvaluationModel } = require('../models');
+const { FacultySelfEvaluationModel, ChairSelfEvaluationModel } = require('../models');
 
 const { authenticateUser, is } = require('../middlewares/auth');
 const { failure, success } = require('../helpers/response');
-const { FACULTY } = require('../constants/roles');
+const { FACULTY, DEAN, CHAIR } = require('../constants/roles');
 
 const router = express.Router();
 
@@ -11,6 +11,16 @@ router.post('/faculty', authenticateUser, is(FACULTY), async (req, res) => {
     try {
         req.body.user = req.user._id;
         const evaluation = await FacultySelfEvaluationModel.create(req.body);
+        return success(res, evaluation);
+    } catch (err) {
+        return failure(res, err);
+    }
+});
+
+router.post('/chair', authenticateUser, is(CHAIR), async (req, res) => {
+    try {
+        req.body.user = req.user._id;
+        const evaluation = await ChairSelfEvaluationModel.create(req.body);
         return success(res, evaluation);
     } catch (err) {
         return failure(res, err);
